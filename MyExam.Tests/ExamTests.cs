bc9c6d5fc608b062e286cfd1d9b4afda32ffa3a5
Adonis <adonis.pso@gmail.com>
2018-10-08 21:17:37 +0300

    Add files via upload

---
+++ MyExam.Tests/ExamTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyExam;
+
+namespace MyExam.Tests
+{
+    [TestClass]
+    public class ExamTests
+    {
+        
+        public TestContext TestContext { get; set; }
+        private Exam exam;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            exam = new Exam();
+        }
+       
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\thema01.csv", "thema01#csv", DataAccessMethod.Sequential), DeploymentItem("thema01.csv"), TestMethod]
+        public void Thema_01()
+        {
+            
+            float first = Convert.ToSingle(TestContext.DataRow["first"]);
+            float second = Convert.ToSingle(TestContext.DataRow["second"]);
+            float expected = Convert.ToSingle(TestContext.DataRow["avg"]);
+            float actual = exam.Average(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+
+     
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\thema02.csv", "thema02#csv", DataAccessMethod.Sequential), DeploymentItem("thema02.csv"), TestMethod]
+        public void Thema_02()
+        {
+            int num = Convert.ToInt32(TestContext.DataRow["numbers"]);
+            int expected = Convert.ToInt32(TestContext.DataRow["max"]);
+            int actual = exam.MinMax(num);
+            Assert.AreEqual(expected, actual);
+        }
+
+  
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\thema03.csv", "thema03#csv", DataAccessMethod.Sequential), DeploymentItem("thema03.csv"), TestMethod]
+        public void Thema_03()
+        {
+            int num = Convert.ToInt32(TestContext.DataRow["numbers"]);
+            String expected = Convert.ToString(TestContext.DataRow["sorted"]);
+            String actual = exam.BubbleSort(num);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\thema04.csv", "thema04#csv", DataAccessMethod.Sequential), DeploymentItem("thema04.csv"), TestMethod]
+        public void Thema_04()
+        {
+            int num = Convert.ToInt32(TestContext.DataRow["numbers"]);
+            int key = Convert.ToInt32(TestContext.DataRow["search"]);
+            String expected = Convert.ToString(TestContext.DataRow["found"]);
+            String actual = exam.BinarySearch(num, key);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyExam/Exam.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExam
+{
+    public class Exam
+    {
+        public float Average(float first, float second)
+        {
+            float sum = first + second;
+            float avg = sum / 2;
+            return avg;
+        }
+
+        public int MinMax(int number)
+        {
+            var numberToArray = number.ToString().Select(t => int.Parse(t.ToString())).ToArray();
+            int min = numberToArray[0];
+            int max = 0;
+            for (int x = 0; x < numberToArray.Length; x++)
+			{
+                if (numberToArray[x] > max)
+                {
+                    max = numberToArray[x];
+                }
+                else if (numberToArray[x] < min)
+                {
+                    min = numberToArray[x];
+                }
+			}
+            return max;
+        }
+
+        public String BubbleSort(int number)
+        {
+            var numberToArray = number.ToString().Select(t => int.Parse(t.ToString())).ToArray();
+            for (int i = 0; i < numberToArray.Length; i++)
+            {
+                
+                for (int x = 0; x < numberToArray.Length - 1; x++)
+                {
+                    if (numberToArray[x] > numberToArray[x + 1])
+                    {
+                        
+                        int temp = numberToArray[x + 1];
+                        ;
+                        numberToArray[x + 1] = numberToArray[x];
+                        numberToArray[x] = temp;
+                        
+                    }
+                } 
+            }
+            string result = String.Join("", numberToArray);
+            return result;
+        }
+
+        public String BinarySearch(int number, int k)
+        {
+            Console.WriteLine("Input Number: " + number);
+            Console.WriteLine("searching for: " + k);
+            var numberToArray = number.ToString().Select(t => int.Parse(t.ToString())).ToArray();
+            string found = "false";
+            int mid = -1;
+            int left = 0;
+            int right = numberToArray.Length - 1;
+
+            while (left <= right)
+            {
+                Console.WriteLine("position of middle: " + mid);
+                Console.WriteLine("left limit: " + left);
+                Console.WriteLine("right limit: " + right);
+                mid = (left + right) / 2;
+                if (numberToArray[mid] == k)
+                {
+                    found = "true";
+                    Console.WriteLine("number " + k + " found!!");
+                    Console.WriteLine("middle number equals to " + k);
+                    Console.WriteLine("Value in the middle position: " + numberToArray[mid]);
+                    Console.WriteLine("position of middle: " + mid);
+                    Console.WriteLine("left limit: " + left);
+                    Console.WriteLine("right limit: " + right);
+                    return found;
+                }
+                else if (numberToArray[mid] > k)
+                {
+                    right = mid - 1;
+                    Console.WriteLine("middle of " + numberToArray[mid] + " is greater than " + k + " store at the right limit middle's position minus 1");
+                    Console.WriteLine("Value in the middle position: " + numberToArray[mid]);
+                    Console.WriteLine("position of middle: " + mid);
+                    Console.WriteLine("left limit: " + left);
+                    Console.WriteLine("right limit: " + right);
+                }
+                else
+                {
+                    left = mid + 1;
+                    Console.WriteLine("middle of " + numberToArray[mid] + " is smaller than " + k + " store at the left limit middle's position plus 1");
+                    Console.WriteLine("Value in the middle position: " + numberToArray[mid]);
+                    Console.WriteLine("position of middle " + mid);
+                    Console.WriteLine("left limit: " + left);
+                    Console.WriteLine("right limit: " + right);
+                }
+                if (found == "false")
+                {
+                    Console.WriteLine("number " + k + " doesn't exist into the input!");
+                }
+            }
+            return found;
+        }
+    }
+}
